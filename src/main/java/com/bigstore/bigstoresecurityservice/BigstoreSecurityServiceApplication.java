bd5ca5ae4d991f79f6b573ebbfacf1760036c2d7
anilyadavrtu <anilyadav.rtu@gmail.com>
2020-01-16 19:54:55 +0530

    base project setup

---
+++ src/main/java/com/bigstore/bigstoresecurityservice/BigstoreSecurityServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bigstore.bigstoresecurityservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigstoreSecurityServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BigstoreSecurityServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bigstore/bigstoresecurityservice/BigstoreSecurityServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bigstore.bigstoresecurityservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BigstoreSecurityServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
